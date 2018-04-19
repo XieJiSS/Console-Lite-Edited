4f2d4fc73acf586e6b61d3d1e0231cc009a83724
JieJiSS <c141028@163.com>
2018-04-19 17:52:52 +0800

    清理工作

--- controller/views/list/list.js
+++ controller/views/list/list.js
@@ -60,13 +60,8 @@ const ListView = Vue.extend({
     },
 
     performAC(text = null) {
-      console.log(text);
-      if(text) {
-        this.editInput = text;
-        console.log(this);
-      } else {
-        this.editInput = this.acList[this.acIndex];
-      }
+      if(text) this.editInput = text;
+      else this.editInput = this.acList[this.acIndex];
       this.updateAC();
       this.acInput.focus();
     },
